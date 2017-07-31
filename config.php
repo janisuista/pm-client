7fb66d3f8cff783d9af371a5de47cfbae0bfd8e9
janisuista <jani.suista@gmail.com>
2017-07-31 23:58:10 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,21 @@
-﻿<?php
+<?php
+// DB_HOST: Tietokannan host-osoite
+define('DB_HOST', '');
+
+// DB_NAME: Tietokannan nimi.
+define('DB_NAME', '');
+
+// DB_USER: Tietokannan äyttäjätunnus. Vaatii SELECT-, UPDATE-, DELETE- ja INSERT-kyselyoikeudet em. tietokantaan.
+define('DB_USER', '');
+
+// DB_PASS: Tietokannan salasana
+define('DB_PASS', '');
+
+// MODE: client vs. admin
+define('MODE', 'client' );
+
+// Sovelluksen juurikansio
+define('ROOT', '');
+
+// Absoluuttinen url (älä vaihda tätä)
+define('PATH', ROOT.MODE);	
